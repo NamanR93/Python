7b99e0162a13fd0373a7da1b3fea32b9cbd5ddf0
NamanR93 <namanrawat90.nr@gmail.com>
2026-04-11 15:51:25 +0530

    variables

---
+++ 0_varibale.py
@@ -0,0 +1,5 @@
+
+Python is dynamically typed language
+# variables have two things well : name and value
+# just like boxes to store data
+#on left varaible name nd on right value thats what stored in box
