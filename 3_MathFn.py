84767a36a8e32684e2124f4eaeae6b0bc980e0ab
NamanR93 <namanrawat90.nr@gmail.com>
2026-04-12 21:54:35 +0530

    Math fun

---
+++ 3_MathFn.py
@@ -0,0 +1,12 @@
+
+import math
+print(round(2.6))
+print(round(2.56456,2))
+print(abs(-2.456)) #always return the +ve value
+print(math.floor(2.9)) # this will give 2
+print(math.ceil(2.9))   # this will give 3
+print(math.pow(9,2))
+
+import random
+
+print(random.randint(1,10))
