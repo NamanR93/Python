d22099d51b50e73556075ee28b7e9a21bd73ad66
NamanR93 <namanrawat90.nr@gmail.com>
2026-04-12 21:54:02 +0530

    Methods

---
+++ 2_Methods.py
@@ -0,0 +1,16 @@
+#methods
+
+name = "naman"
+print(len(name))
+print(name.upper())
+print(name.find('a'))
+print(name.replace('a','e'))
+print('e' in name)
+print(name.title())  #returns string where every word is uppercase
+
+
+#operators
+#precedence PEMDAS
+
+
+# NONE is an object the represent the absence of value
