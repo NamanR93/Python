26391b93cf3fd9ef593d14aadd7db2f57bdbb2db
NamanR93 <namanrawat90.nr@gmail.com>
2026-04-13 16:20:42 +0530

    Loops

---
+++ 5_Loops.py
@@ -0,0 +1,16 @@
+
+# i = 1
+# while(i<=5):
+#     print(i)
+#     i+=1
+# print("done")
+
+price = [10,20,30]
+sum = 0
+for i in price:
+    sum +=i
+print(f"the total price of items is {sum}")  
+
+for x in range(3):
+    for y in range(2):
+        print(f"({x},{y})")
