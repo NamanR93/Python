b8b94c7edfbc8e66479d878a522ad82d4af7ab27
NamanR93 <namanrawat90.nr@gmail.com>
2026-04-21 10:53:06 +0530

    Sets

---
+++ 11_sets.py
@@ -0,0 +1,12 @@
+setss = {"naman", 1,"banana"}
+
+'''
+union .union() or use |
+can join set with tuple
+.update() : merges the set1 into set2
+both update and union remove duplicates
+
+s1.issubset(s2)  : 
+a.disjoint(b)  : checks two elements in common
+
+'''
