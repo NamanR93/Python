97feca1baaa5baea55da73a2fab1521f6b2190f9
NamanR93 <namanrawat90.nr@gmail.com>
2026-04-21 10:53:29 +0530

    questions

---
+++ Practise.py
@@ -0,0 +1,14 @@
+#largest in list
+# add,prod
+#hello world
+#matching word
+#sum of even
+#conv into dict
+
+#longest len
+for i in range(n):
+    ele = input()
+    list.append(ele)
+    if(len(ele)>max):
+        max = len(ele)
+print(max)
